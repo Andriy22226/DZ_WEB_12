cae73f1e74485587d72cad5e2f9c7209190e608d
Andriy <127453425+Andriy22226@users.noreply.github.com>
2023-10-15 13:25:30 +0300

    Add files via upload

---
+++ dz_12_web/src/routes/auth.py
@@ -0,0 +1,52 @@
+import logging
+from typing import List
+
+from fastapi import APIRouter, HTTPException, Depends, Path, Query, status, Security
+from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
+
+from src.database.db import get_db
+from src.schemas import UserResponse, UserModel, TokenModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repository import users as repository_users
+from src.services.auth import auth_service
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+security = HTTPBearer()
+
+
+@router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def signup(body: UserModel, db: AsyncSession = Depends(get_db)):
+    exist_user = await repository_users.get_user_by_email(body.email, db)
+    if exist_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exist")
+    body.password = auth_service.password_manager.get_password_hash(body.password)
+    new_user = await repository_users.create_user(body, db)
+    return new_user
+ 
+
+@router.post("/login", response_model=TokenModel, status_code=status.HTTP_200_OK)
+async def login_user(body: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    user = await repository_users.get_user_by_email(body.username, db)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    if not auth_service.password_manager.verify_password(body.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+    access_token = await auth_service.create_access_token(user)
+    refresh_token = await auth_service.create_refresh_token(user)
+    await repository_users.update_token(user, refresh_token, db)
+    return {'access_token': access_token, 'refresh_token': refresh_token, 'token_type': 'bearer'}
+
+
+@router.get('/refresh_token', response_model=TokenModel)
+async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security),
+                        db: AsyncSession = Depends(get_db)):
+    token = credentials.credentials
+    user = await auth_service.authorised_user(token, db)
+    if user.refresh_token != token:
+        await repository_users.update_token(user, None, db)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
+    access_token = await auth_service.create_access_token(user)
+    refresh_token = await auth_service.create_refresh_token(user)
+    await repository_users.update_token(user, refresh_token, db)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
